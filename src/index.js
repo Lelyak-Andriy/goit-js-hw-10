18695d1cf7b7500a81d7d411abe0a2bbdb391ee2
Lelyak-Andriy <a.lelyak47@gmail.com>
2021-12-05 18:32:01 +0200

    5

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import './css/styles.css';
 import countriesCardTpl from './templates/countries-card.hbs'
+import debounce from 'lodash.debounce'
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
-countryContainer: document.querySelector(".country-info")
+    countryContainer: document.querySelector(".country-info"),
+    searchInput: document.querySelector("#search-box"),
 }
 
-fetch('https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages')
-.then(response => {
-    return response.json()
-})
-    .then(country => {
-        console.log(country);
-        const markup = countriesCardTpl(country);
-        // console.log(markup);
-        refs.countryContainer.innerHTML = markup;
-    })
-    .catch(error => {
-        console.log(error);
-})
+refs.searchInput.addEventListener('input', debounce(countrySearchInputHandler, DEBOUNCE_DELAY));
+
+function countrySearchInputHandler(e) {
+    e.preventDefault();
+    
+    const searchQuery = e.target.value;
+    
+
+    fetchCountries(searchQuery)
+        .then(renderCountryCard)
+        .catch(error => console.log(error));
+}
+  
+
+
+function fetchCountries() {
+    return fetch('https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages')
+        .then(response => {
+            return response.json()
+        });
+}
+
+    function renderCountryCard(country) {
+    const markup = countriesCardTpl(country);
+    // console.log(markup);
+    refs.countryContainer.innerHTML = markup;
+    }
+
+//     function clearCountryContainer() {
+//   refs.countryContainer.innerHTML = '';
+// }
