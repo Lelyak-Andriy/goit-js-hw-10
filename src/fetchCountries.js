8b9c45e47b328e347478ad7dbda56a019a786e2a
Lelyak-Andriy <a.lelyak47@gmail.com>
2021-12-05 17:18:00 +0200

    4

---
+++ src/fetchCountries.js
@@ -0,0 +1,1 @@
+fetchCountries(name)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
-
+import countriesCardTpl from './templates/countries-card.hbs'
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+countryContainer: document.querySelector(".country-info")
+}
+
+fetch('https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages')
+.then(response => {
+    return response.json()
+})
+    .then(country => {
+        console.log(country);
+        const markup = countriesCardTpl(country);
+        // console.log(markup);
+        refs.countryContainer.innerHTML = markup;
+    })
+    .catch(error => {
+        console.log(error);
+})
